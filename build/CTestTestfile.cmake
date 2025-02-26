186098ae91c7e783ae527a1ca36da3e4821d3927
Destinn <donats2004@gmail.com>
2025-02-26 18:27:37 +0300

    chore: remove generated CMake files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/destinn/Documents/code/problem_hwe
-# Build directory: /Users/destinn/Documents/code/problem_hwe/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_sieve "/Users/destinn/Documents/code/problem_hwe/build/test_sieve")
-set_tests_properties(test_sieve PROPERTIES  _BACKTRACE_TRIPLES "/Users/destinn/Documents/code/problem_hwe/CMakeLists.txt;14;add_test;/Users/destinn/Documents/code/problem_hwe/CMakeLists.txt;0;")
